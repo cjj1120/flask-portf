f372e1eab97da7c7ebef25b923a94486d4d8a0d7
CJJ <cjj8168@gmail.com>
2021-06-27 00:27:40 +0800

    web3

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -b :$PORT app:hello
+web: gunicorn app:hello
--- hello.py
+++ hello.py
@@ -1,11 +1,7 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-import os
-...
-port = int(os.environ.get('PORT', 5000))
-...
-app.run(host='0.0.0.0', port=port, debug=True)
+
 
 # Create Index Page
 @app.route('/')
@@ -21,5 +17,8 @@ def index():
 def about():
 	return render_template('test.html')
 
+if __name__ == "__main__":
+	app.run()
 
+ 
 
