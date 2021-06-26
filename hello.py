61ef5c991942baf958287a065c93a302773d0a1b
CJJ <cjj8168@gmail.com>
2021-06-27 00:21:01 +0800

    web2

--- hello.py
+++ hello.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
+import os
+...
+port = int(os.environ.get('PORT', 5000))
+...
+app.run(host='0.0.0.0', port=port, debug=True)
 
 # Create Index Page
 @app.route('/')
